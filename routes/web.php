9bbb646041783cedcad5ddac842994a421b45feb
trunghn2003 <hvthn126@gmail.com>
2024-07-17 14:05:22 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -16,22 +16,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
+   
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('users', UserController::class);
+    Route::resource('income-categories', IncomeCategoryController::class);
+    Route::resource('incomes', IncomeController::class);
+    Route::resource('expense-categories', ExpenseCategoryController::class);
+    Route::resource('expenses', ExpenseController::class);
+    Route::get('/statistics', [StatisticsController::class, 'index'])->name('statistics.index');
 });
-
 require __DIR__.'/auth.php';
-
-
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::resource('users', UserController::class);
-Route::resource('income-categories', IncomeCategoryController::class);
-Route::resource('incomes', IncomeController::class);
-Route::resource('expense-categories', ExpenseCategoryController::class);
-Route::resource('expenses', ExpenseController::class);
-Route::get('/statistics', [StatisticsController::class, 'index'])->name('statistics.index');
-

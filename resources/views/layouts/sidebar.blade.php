7f94d4c0a67016c92723f6f050f9bee2c7d3f055
trunghn2003 <hvthn126@gmail.com>
2024-07-17 10:18:51 +0700

    add sidebar

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,16 @@
+<nav class="sidebar">
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('expense-categories.index') }}">Expense Categories</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('expenses.index') }}">Expenses</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('income-categories.index') }}">Income Categories</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('incomes.index') }}">Incomes</a>
+        </li>
+    </ul>
+</nav>
